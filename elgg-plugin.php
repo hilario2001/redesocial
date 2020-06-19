b67a38a790e13d4ed45434327e68f9a1b8318455
hilario2001 <67173895+hilario2001@users.noreply.github.com>
2020-06-20 00:59:52 +0100

    Add files via upload

---
+++ elgg-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+use Pessek\PessekChat\Bootstrap;
+use Pessek\PessekChat\PessekPrcXmlrpcElgg;
+use Pessek\PessekChat\PessekPrcConnector;
+
+/**
+ * Friend Request
+**/
+return [
+	'bootstrap' => \Pessek\PessekChat\Bootstrap::class
+];
